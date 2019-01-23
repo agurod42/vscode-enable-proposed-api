9bba7a27007826ab28957229c0de22e63de38155
Agustín Rodríguez <me@agurodriguez.net>
2019-01-23 14:05:11 -0500

    Ask for confirmation before restarting Code

--- src/extension.ts
+++ src/extension.ts
@@ -6,14 +6,13 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('extension.restartAndEnableProposedApi', () => {
-        try {
-            const packageJson = findElegiblePackageJson();
-            const restartCommand = `ps -e -o pid,command | grep 'Visual Studio Code.app' | cut -d ' ' -f 1 | xargs -L1 kill -9 & sleep 1 & code -n --enable-proposed-api ${packageJson.publisher}.${packageJson.name} ${vscode.workspace.rootPath}`;
-            spawn('sh', ['-c', `osascript -e "tell application \\"Terminal\\" to do script \\"${restartCommand}\\""`]);
-        }
-        catch (err) {
-            console.log(err);
-        }
+        vscode.window
+            .showWarningMessage('This command will forcefully close all VS Code windows. Are you sure you want to continue?', ...['Yes', 'No'])
+            .then((value) => {
+                if (value === 'Yes') {
+                    restartAndEnableProposedApi();
+                }
+            });
     });
 
     context.subscriptions.push(disposable);
@@ -24,6 +23,17 @@ function findElegiblePackageJson(): any {
     const packageJson = JSON.parse(readFileSync(packageJsonFilePath).toString());
     if (!packageJson.enableProposedApi) throw new Error('The current project doesn\'t use the Proposed API');
     return packageJson;
+}
+
+function restartAndEnableProposedApi(): void {
+    try {
+        const packageJson = findElegiblePackageJson();
+        const restartCommand = `ps -e -o pid,command | grep 'Visual Studio Code.app' | cut -d ' ' -f 1 | xargs -L1 kill -9 & sleep 1 & code -n --enable-proposed-api ${packageJson.publisher}.${packageJson.name} ${vscode.workspace.rootPath}`;
+        spawn('sh', ['-c', `osascript -e "tell application \\"Terminal\\" to do script \\"${restartCommand}\\""`]);
+    }
+    catch (err) {
+        vscode.window.showErrorMessage(err);
+    }
 }
 
 export function deactivate() {
